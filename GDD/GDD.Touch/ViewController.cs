8f925de9d651b4c0359df0662fe5ceecee0b5306
Alex Stefan <alex.steefan@gmail.com>
2018-06-03 02:47:47 +0300

    Add button in iOS to match Android project

--- GDD/GDD.Touch/ViewController.cs
+++ GDD/GDD.Touch/ViewController.cs
@@ -5,7 +5,9 @@ using UIKit;
 namespace GDD.Touch
 {
     public partial class ViewController : UIViewController
-    {
+	{
+        int count = 1;
+
         protected ViewController(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -22,5 +24,10 @@ namespace GDD.Touch
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
+
+		partial void ButtonClick(UIButton sender)
+		{
+			sender.SetTitle($"{count++} clicks!", UIControlState.Normal); 
+		}
     }
 }
